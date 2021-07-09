aacabaf73daef89190f45bee66b7e1637a1d6ab9
Gunta <gunta.satkuna@gmail.com>
2021-07-09 20:30:10 +0300

    first commit from Gunta

---
+++ src/Team2.java
@@ -0,0 +1,14 @@
+public class Team2 {
+    public static void main(String[] args) {
+        short number1 = 37;
+        short number2 = 23;
+        if (number1 > number2) {
+            System.out.println(number1 + " is bigger");
+            System.out.println(number2 + " is smaller");
+        } else {
+            System.out.println(number2 + " is bigger");
+            System.out.println(number1 + " is smaller");
+        }
+
+    }
+}
