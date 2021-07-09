d1445168925949f67a189fde204bf490c8c13025
martins.zalitis <martins.zalitis@gmail.com>
2021-07-09 18:22:48 +0300

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,36 @@
+public class Task1 {
+    /**
+     Task 01 - conditions, comparison, basic calculation (45 min)
+
+     Write a program to compare two elements:
+
+     ●      Create two variables with the type short.
+
+     ●      Initialize the variables with the values as You want (no Input from user needed!)
+
+     ●      Verify which variable is the largest. Print out information in the console.
+
+     ●      Verify which variable is the smallest. Print out information in the console.
+
+     ●      Verify if both variables are equals. Print out information in the console.
+
+     ●      Verify if there are any variables which are odd (uneven). Print out information in the console.
+
+     ●      Verify if there are any variables which are even. Print out information in the console.
+
+     ●      Verify if there are any variables which are negative. Print out information in the console.
+
+     ●      Verify if there are any variables which are positive. Print out information in the console.
+
+     ●      Verify if there are any variables which are less than 100. Print out information in the console.
+
+
+     ●      Calculate sum of two elements, round to whole numbers and print information in the console.
+
+     ●      Calculate differences between both numbers, output it as positive number with two numbers after the coma
+
+     * */
+    public static void main(String[] args) {
+        System.out.println("I am the Java program.");
+    }
+}
