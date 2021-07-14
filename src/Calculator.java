9f19157fdf56902ec3aed5ef66406d9ac850f1d5
martins.zalitis <martins.zalitis@gmail.com>
2021-07-14 20:30:31 +0300

    Calculator task

---
+++ src/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+/**
+ Write a program to do multiple calculation options with two numbers:
+
+ ●      Create two numerical variables with initial values.
+
+ ●      Create an char type variable which represents the user option (please use the switch control):
+
+ a.      ‘+’ sum both elements;
+
+ b.     ’-’: do subtraction;
+
+ c.      ’/’: do dividing;
+
+ d.     ‘*’: do multiplication;
+
+ e.      ‘%’: get remainder when the first element is divided by the second element;
+
+ f.      ‘p’: print out both elements;
+
+ g.     ‘b’: verify which element is bigger;
+
+ h.     ‘s’:verify which element is smaller;
+
+ ●      * additionally - please do add the possibility to write both variable values and the option in the console. Read, verify and do an algorithm
+
+ ●
+ * */
+}
