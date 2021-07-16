a91988eacebea389fd68ccd741f516d560372fdb
martins.zalitis <martins.zalitis@gmail.com>
2021-07-16 20:35:44 +0300

    Array1stTask

---
+++ src/Array1stTask.java
@@ -0,0 +1,35 @@
+public class Array1stTask {
+    /**
+
+     Practical part 1 (individual, 30-60 min):
+
+     1. Create an array with int type which consists of 5 elements
+
+     2. initialize values.
+
+     3. Print first, last and some other element from array.
+
+     4. Calculate and output sum of all elements in the array.
+
+     5. print length of the array.
+
+     6. Print all five elements using for loop
+
+     <bonus>
+     7. Change program - initialization of values should be taken from user's input.
+
+
+
+     P.S. Example of for loop:
+     //outputting all values in array with loop
+     int[] myList3 = {110, 55, 50};
+     System.out.println(myList3[0]);
+     System.out.println(myList3[1]);
+     System.out.println(myList3[2]);
+
+     System.out.println(" *********** ");
+     for (int i = 0; i < 3; i++) {
+     System.out.println(myList3[i]);
+     }
+     */
+}
