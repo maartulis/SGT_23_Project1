2cbe1d6b5003845df8c5e8337ad8557f98fb242e
SantaKolosovska <santa.kolosovska@gmail.com>
2021-07-16 18:38:50 +0300

    Santa, Anna Borne, Alina - some changes, now it runs

--- src/Santa_Anna_BAlina.java
+++ src/Santa_Anna_BAlina.java
@@ -1,105 +1,92 @@
 import java.util.Scanner;
 public class Santa_Anna_BAlina {
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-//    *
-//     Write a program to do multiple calculation options with two numbers:
-//
+
 //     ●      Create two numerical variables with initial values.
 
 //        int x = 5;
 //        int y = 3;
 
 
+        System.out.println("Please write two numbers: ");
+        float x = scanner.nextFloat();
+        float y = scanner.nextFloat();
+        System.out.println("Please write the option +, -, /, *, %, p (print out), b (which one is bigger), s (which one is smaller: ");
 
-       System.out.println("Please write two numbers: ")
-     int x = scanner.nextInt();
-     int y = scanner.nextInt();
-        System.out.println("Please write the option +, -, /, *, %, p (print out), b (which one is bigger), s (which one is smaller: ")
-        char myLetter = scanner.charAt(0);
-//
-//
+        String str = scanner.next();
+        char op = str.charAt(0);
+
+
 //     ●      Create an char type variable which represents the user option (please use the switch control):
 
 
-            char myLetter = '+';
-            char myLetter = '-';
-            char myLetter = '/';
-            char myLetter = '*';
-            char myLetter = '%';
-            char myLetter = 'p';
-            char myLetter = 'b';
-            char myLetter = 's';
+//        char op = '+';
+//        char op = '-';
+//        char op = '/';
+//        char op = '*';
+//        char op = '%';
+//        char op = 'p';
+//        char op = 'b';
+//        char op = 's';
 
 
-//     a.      ‘+’ sum both elements;
 
-                switch (myLetter) {
-                case '+':
-                System.out.println(x+y);
-                }
-                break;
-//
-//     b.     ’-’: do subtraction;{
 
-                case '-':
-                System.out.println(x-y);
-                break;
-//
-//     c.      ’/’: do dividing;
+        switch (op) {
 
-                case '/':
-                System.out.println(x/y);
+            case '+':                   //     a.      ‘+’ sum both elements;
+                System.out.println(x + y);
                 break;
 
-//
-//     d.     ‘*’: do multiplication;
 
+            case '-':                   //     b.     ’-’: do subtraction;{
 
-                case '*':
-                System.out.println(x*y);
+                System.out.println(x - y);
                 break;
-//
-//     e.      ‘%’: get remainder when the first element is divided by the second element;
 
 
-                case '%':
-                System.out.println(x%y);
+            case '/':                   //     c.      ’/’: do dividing;
+                System.out.println(x / y);
                 break;
-//
-//     f.      ‘p’: print out both elements;
 
 
-                case 'p':
-                System.out.println(x + "and" + y);
+            case '*':                   //     d.     ‘*’: do multiplication;
+                System.out.println(x * y);
                 break;
-//
-//     g.     ‘b’: verify which element is bigger;
 
 
-                case 'b':
-                if (x>y) {
-                System.out.println(x + " is the biggest value");
+            case '%':                   //     e.      ‘%’: get remainder when the first element is divided by the second element;
+                System.out.println(x % y);
+                break;
+
+
+            case 'p':                   //     f.      ‘p’: print out both elements;
+                System.out.println(x + " and " + y);
+                break;
+
+
+            case 'b':                   //     g.     ‘b’: verify which element is bigger;
+                if (x > y) {
+                    System.out.println(x + " is the biggest value");
                 } else {
-                System.out.println(y + " is the biggest value");
+                    System.out.println(y + " is the biggest value");
                 }
-//
-//     h.     ‘s’:verify which element is smaller;
+                break;
 
-                    case 's':
-                    if (x>y) {
-                        System.out.println(y + " is the smallet value");
-                    } else {
-                        System.out.println(x + " is the smallet value");
-                    }
+
+            case 's':                   //     h.     ‘s’:verify which element is smaller;
+                if (x > y) {
+                    System.out.println(y + " is the smallest value");
+                } else {
+                    System.out.println(x + " is the smallest value");
+                }
+                break;
+
+
+        }
 
 
     }
-
-
-//
-//     ●      * additionally - please do add the possibility to write both variable values and the option in the console. Read, verify and do an algorithm
-//
-//     ●
-//     *
 }
