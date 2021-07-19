a1c19e56a3cceb370b27bb1473a6e4bbfe851f93
Laila Duffy <lail.duffy@gmail.com>
2021-07-19 18:49:12 +0100

    Unfinished Task 2, still thinking

---
+++ src/LailaDArrays.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class LailaDArrays {
+    public static void main(String[] args) {
+        //Task1
+        int[] randomNumbers = {3, 11, 81, 20, -4, 0};
+        System.out.println("Array in normal order");
+        for (int i = 0; i < randomNumbers.length; i++) {
+            System.out.println(randomNumbers[i]);
+        }
+
+        System.out.println("Array in reverse order");
+        for (int i = randomNumbers.length - 1; i >= 0; i--) {
+            System.out.println(randomNumbers[i]);
+        }
+        //Task2
+        System.out.println("-------------------------------------");
+        int[] anotherArray = {30, 15, 15, 3, 15, 12, 13};
+        Arrays.sort(anotherArray);
+        int maxVal = anotherArray[anotherArray.length - 1];
+        for (int i = maxVal; i < anotherArray.length; i++) {
+            if (anotherArray[i] < maxVal) {
+                maxVal = anotherArray[i];
+                i--;
+            }
+        }
+
+        System.out.println("The second largest number in the array is " + maxVal);
+        System.out.println("The lowest number in the array is " + anotherArray[0]);
+
+    }
+}
+
