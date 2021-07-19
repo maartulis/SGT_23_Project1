af30c663aeb719dcb6eaaee47de00fb4471f814a
SantaKolosovska <santa.kolosovska@gmail.com>
2021-07-19 19:53:59 +0300

    first push

---
+++ src/SantaKolosovska.java
@@ -0,0 +1,15 @@
+public class SantaKolosovska {
+    public static void main(String[] args) {
+        int[] myArray = {1, 6, 3, 50, 3, 12};
+
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
+
+        for (int i = myArray.length - 1; i >= 0; i--) {
+            System.out.println(myArray[i]);
+        }
+
+
+    }
+}
