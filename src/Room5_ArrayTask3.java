2b08d76c93cc6233134e9fb16fd5df264781915f
Gunta <gunta.satkuna@gmail.com>
2021-07-19 21:26:34 +0300

    Second commit from Gunta

--- src/Room5_ArrayTask3.java
+++ src/Room5_ArrayTask3.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 public class Room5_ArrayTask3 {
+
         public static void main(String[] args) {
-
-
                 Scanner scan = new Scanner(System.in);
                 System.out.println("Your array is:");
                 System.out.println("Enter number 1: ");
@@ -14,10 +13,11 @@ public class Room5_ArrayTask3 {
                 System.out.println("Enter number 4: ");
                 int number4 = scan.nextInt();
 
-
-
                 int[] myList = {number1, number2, number3, number4};
-        }
-}
 
+                int sum = (number1+number2+number3+number4);
+                double average = sum / myList.length;
+                System.out.println("Average value of the array elements is : " + average);
+                        }
+                }
 
