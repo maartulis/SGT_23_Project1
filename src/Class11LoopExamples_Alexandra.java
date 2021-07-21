f978267a095d731bbd6c2b2e560bc623bbefab02
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-07-21 21:03:30 +0300

    Class 11 Task 2 - by Alexandra

--- src/Class11LoopExamples_Alexandra.java
+++ src/Class11LoopExamples_Alexandra.java
@@ -2,26 +2,37 @@ public class Class11LoopExamples_Alexandra {
     public static void main(String[] args) {
 //Task 1
 
-        int x=10;
+        int x = 10;
 
         //WHILE
         System.out.println("***WHILE LOOP***");
-        while (x>=(-10)) {
+        while (x >= (-10)) {
             System.out.println("Value of x number is " + x);
             x--;
         }
 
         //WHILE DO
-        x=10;
+        x = 10;
         System.out.println("*** DO WHILE LOOP***");
-        do {System.out.println("Value of x number is " + x);
+        do {
+            System.out.println("Value of x number is " + x);
             x--;
-        } while (x>=(-10));
+        } while (x >= (-10));
 
         //FOR
         System.out.println("*** FOR ***");
-        for (int i=10; i>=(-10); i--) {
-            System.out.println("Value of x number is " + i);
+        for (int i = 10; i >= (-10); i--) {
+            System.out.println("Value of i number is " + i);
+        }
+
+//Task 2
+        System.out.println("*** TASK 2 - FOR LOOP ***");
+        for (int i = 1; i <= 20; i = i + 2) {
+            System.out.print(i + " ,");
+            if(i >= 19){
+                System.out.println("");
+                break;
+            }
         }
     }
 }
