f6f3237645af99a13d161f00346fc28608104ced
annab <annabo@inbox.lv>
2021-07-23 08:51:15 +0300

    ForLoop task

---
+++ src/AnnaBForloop.java
@@ -0,0 +1,37 @@
+public class AnnaBForloop {
+
+        public static void main(String[] args) {
+            // write your code here
+            for (int i = -10; i <= 10; i++){
+                System.out.println("Value of i: " + i);
+            }
+            System.out.println("*********************");
+
+            int x = -10;
+
+            //while loop example:
+            while (x <= 10){
+                System.out.println("Value of x: " + x);
+                x++;
+            }
+            System.out.println("*********************");
+
+            //do while example:
+            x = -10;
+            do {
+                System.out.println("Value of x: " + x);
+                x++;
+            } while (x <= 10);
+
+            System.out.println("----------");
+
+            for (int i = 0; i <=20; i++) {
+                {
+                    System.out.println("value of i:" + i);
+
+                }
+            }
+        }
+    }
+
+
