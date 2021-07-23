2cc783f7b3d4a66456f04949010e2d2f7d960c88
SantaKolosovska <santa.kolosovska@gmail.com>
2021-07-23 17:24:09 +0300

    Class 11 Loops (for, while, do while, no comma after the last element)

--- src/Class11LoopsSanta.java
+++ src/Class11LoopsSanta.java
@@ -27,47 +27,37 @@ public class Class11LoopsSanta {
 
         System.out.println("SECOND TASK*****************");
 
+        System.out.println(" ");
+        System.out.println("--- 2. for loop ---");
 
-        for (int a = 1; a < 20; a = a + 2){
-            System.out.print(a + ", ");
-        }
+        int a = 0;
+        for (a = 1; a < 20 - 2; a = a + 2){
+                System.out.print(a + ", ");
+                }
+        System.out.println(a);
 
-        System.out.println("--- 2. count ---");
-
-
-        int count = 0;
-        for (int a = 1; a < 20; a = a + 2){
-            count++;
-        }
-        System.out.println(count);
-        int [] arrayX = new int [count];
-
-        for (int a = 1; a < 20; a = a + 2){
-            arrayX[a-2] = a++;
-        }
-
-        System.out.println("arrayX size is " + arrayX.length);
-        System.out.println(arrayX[1]);
-
-
-
-
-//        int x;
-//        int aa = 1;
-//        for (aa; aa <= 20; aa = aa + 2){
-//            aa
-//
-//        }
 
         System.out.println(" ");
         System.out.println("--- 2. while loop ---");
 
         int b = 1;
+        System.out.print(b);
         while(b<20){
-            System.out.print(b + ", ");
             b = b + 2;
+            if (b>20){
+                break;
+            }
+            System.out.print(", " + b);
         }
-        System.out.println(); //sout
+
+
+
+//        int count = 0;
+//        for (int a = 1; a < 20; a = a + 2){
+//            count++;
+//        }
+//
+//        int [] arrayX = new int [count];
 
 
 
